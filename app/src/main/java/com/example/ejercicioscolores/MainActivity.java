4324cd1bc489dd00b27db4a857c166c7da2b89b4
Juan Luis <juanluperea@hotmail.com>
2019-05-16 13:14:42 +0200

    Version con Boton

--- app/src/main/java/com/example/ejercicioscolores/MainActivity.java
+++ app/src/main/java/com/example/ejercicioscolores/MainActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
@@ -13,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     private int color;
     private int numtoques;
     private long tiempo;
+    private ImageButton boton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,21 +23,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         this.color = getResources().getColor(R.color.negro);
         this.numtoques = 0;
-
-
-
+        this.boton = findViewById(R.id.empezarBTN);
     }
 
 
     public void cambiarColor(View v) {
         LinearLayout cajatocada = (LinearLayout) v;
 
-        if (v.getTag() == null) {
+        if (v.getTag() == null && this.boton.getVisibility() == View.INVISIBLE) {
             Log.d("TOCADO", v.getBackground().toString());
             v.setTag(true);
             cajatocada.setBackgroundColor(this.color);
             numtoques++;
-            if (numtoques == 1) tiempo = System.currentTimeMillis();
+         //   if (numtoques == 1) tiempo = System.currentTimeMillis();
             if (numtoques == 6) {
                 tiempo = System.currentTimeMillis() - tiempo;
                 Double tiempodbl = tiempo / 1000d;
@@ -56,5 +57,17 @@ public class MainActivity extends AppCompatActivity {
             this.finishAffinity();
         }
     }
+
+    public void empezarPartida (View v) {
+        Log.d("MIAPP", "Empezar la partida");
+        // TODO iniciar el crono, quitar el botón
+        tiempo = System.currentTimeMillis();
+        boton.setVisibility(View.INVISIBLE);
+        Toast.makeText(this, "Comienza el juego!!", Toast.LENGTH_SHORT).show();
+
+
+    }
+
+
 
 }
